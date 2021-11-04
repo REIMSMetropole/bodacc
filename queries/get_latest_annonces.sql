7fefe642ac02e75b3731adb9c182bdc34d2cecd0
PLM <pierre.muckensturm@reimsmetropole.fr>
2021-11-04 19:43:30 +0100

    Update get_latest_annonces.sql
    à approuver si vous voulez tous les jugements. Sinon, revoir la regex

--- queries/get_latest_annonces.sql
+++ queries/get_latest_annonces.sql
@@ -6,21 +6,6 @@ SELECT RCS,DATE,EFFECTIFS, NATURE from annonces
     AND uniteslegales.EFFECTIFS != 'NN'
     AND uniteslegales.EFFECTIFS >= '01'
     -- filtrer les sociétés civiles immobilières
-    AND uniteslegales.CATEGORIEJURIDIQUE != '6540'
-    AND uniteslegales.CATEGORIEJURIDIQUE != '6541'
-    AND uniteslegales.CATEGORIEJURIDIQUE != '6542'
-    AND uniteslegales.CATEGORIEJURIDIQUE != '6543'
-    AND uniteslegales.CATEGORIEJURIDIQUE != '6544'
-    AND (
-        NATURE = 'jugement d''ouverture de liquidation judiciaire'
-        OR NATURE = 'jugement de clôture pour insuffisance d''actif'
-        OR NATURE = 'jugement de conversion en liquidation judiciaire'
-        OR NATURE = 'jugement d''extension de liquidation judiciaire'
-        OR NATURE = 'jugement de clôture de la liquidation des biens pour insuffisance d''actif'
-        OR NATURE = 'jugement d''extension d''une procédure de redressement judiciaire'
-        OR NATURE = 'jugement de clôture pour insuffisance d''actif et autorisant la reprise des poursuites individuelles'
-        OR NATURE = 'jugement de conversion en liquidation judiciaire de la procédure de sauvegarde'
-        OR NATURE = 'jugement autorisant la reprise des poursuites individuelles des créanciers'
-        OR NATURE = 'jugement de conversion en liquidation judiciaire de la procédure de sauvegarde financière accélérée'
-    )
+    and uniteslegales.CATEGORIEJURIDIQUE <> all (array['6540', '6541', '6542','6543','6544'])
+    AND (NATURE ~* 'jugement')
     ORDER BY EFFECTIFS DESC;
